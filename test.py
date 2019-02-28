44e74deec6efc87d8c63be6596ff65e9c1747e97
Fenix <shajia111@126.com>
2019-02-28 15:33:18 +0800

    修改默认光标在data_Entry上

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import tkinter
+root=tkinter.Tk()
 
+scrolly=tkinter.Scrollbar(root)
+scrolly.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+mylb=tkinter.Listbox(root,yscrollcommand=scrolly.set)
+mylb.pack()
+
+
+scrolly.config(command=mylb.yview)
+tkinter.mainloop()
